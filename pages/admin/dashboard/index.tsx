f233b6d4855d8c0950f4be72c6fc5131ebb1e4d2
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2024-08-27 14:13:39 +0530

    0.0.1 V

--- pages/admin/dashboard/index.tsx
+++ pages/admin/dashboard/index.tsx
@@ -4,11 +4,7 @@ import Head from 'next/head';
 import PageWrapper from '../../../layout/PageWrapper/PageWrapper';
 import { demoPagesMenu } from '../../../menu';
 import Page from '../../../layout/Page/Page';
-import Chart1 from "./chart1"
-import Chart2 from "./chart2"
-import Telescope from './telescope'
-import Revenue from './revenue';
-import Supply from './SupplyPredict'
+
 import Chart3 from'./top-product-chart'
 
 const Index: NextPage = () => {
@@ -21,19 +17,9 @@ const Index: NextPage = () => {
 
       <Page container='fluid'>
         <div className='row'>
+       
           <div className='col-9'>
-            <Chart1 />
-          </div>
-          <div className='col-3'>
             
-            <Supply />
-          </div>
-          <div className='col-xxl-3'>
-            <Telescope />
-            <Revenue />
-          </div>
-          <div className='col-9'>
-            <Chart2 />
             <Chart3/>
           </div>
 
