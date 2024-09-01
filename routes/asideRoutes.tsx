e2f1b10bbc120eddf752986762944d0066b6ccdb
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2024-09-02 00:35:55 +0530

    0.0.14

--- routes/asideRoutes.tsx
+++ routes/asideRoutes.tsx
@@ -4,7 +4,7 @@ import { demoPagesMenu, pageLayoutTypesPagesMenu } from '../menu';
 
 
 const AdminAside = dynamic(() => import('../pages/_layout/_asides/AdminAside'));
-const OfficeAside = dynamic(() => import('../pages/_layout/_asides/OfficeAsider'));
+
 const ViewAside = dynamic(() => import('../pages/_layout/_asides/ViewAside'));
 const CashierAside = dynamic(() => import('../pages/_layout/_asides/CashierAsider'));
 
@@ -13,7 +13,7 @@ const CashierAside = dynamic(() => import('../pages/_layout/_asides/CashierAside
 const asides = [
 	
 	{ path: '/admin/*', element: <AdminAside/>, exact: true },
-	{ path: '/production-coordinator/*', element: <OfficeAside/>, exact: true },
+
 	{ path: '/cashier/*', element: <CashierAside/>, exact: true },
 	{ path: '/viewer/*', element: <ViewAside/>, exact: true },
 
