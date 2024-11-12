bba1951a1fd684adc22c2a7589713353f3989265
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2024-11-12 11:31:34 +0530

    0.0.29

--- redux/slices/stockInAPISlice.ts
+++ redux/slices/stockInAPISlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const lotInApiSlice = createApi({
   reducerPath: 'lotApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://richfabricinventory.netlify.app/api/' }),
   tagTypes: ['Lot'],
   endpoints: (builder) => ({
     // Fetch all lots
--- redux/slices/stockOutApiSlice.ts
+++ redux/slices/stockOutApiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const stockOutApiSlice = createApi({
   reducerPath: 'stockOutApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://richfabricinventory.netlify.app/api/' }),
   tagTypes: ['StockOut'],
   endpoints: (builder) => ({
     // Fetch all stock outs
